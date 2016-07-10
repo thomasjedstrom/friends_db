6f3f11c22a30f80256651221c46684e8e64c563f
Thomas Edstrom <thomasjedstrom@gmail.com>
2016-07-09 21:23:03 -0500

    got the edit page working (at least it will render).  The edit form still needs some tweeking to get it functional though.

--- server.py
+++ server.py
@@ -11,20 +11,51 @@ def index():
 
 @app.route('/friends', methods=['POST'])
 def create():
-    query = "INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at) VALUES (:first_name, :last_name, :occupation, NOW(), NOW())"
+    query = "INSERT INTO friends (first_name, last_name, email, created_at, updated_at) VALUES (:first_name, :last_name, :email, NOW(), NOW())"
     data = {
              'first_name': request.form['first_name'], 
              'last_name':  request.form['last_name'],
-             'occupation': request.form['occupation']
+             'email': request.form['email']
            }
     mysql.query_db(query, data)
     return redirect('/')
 
-@app.route('/friends/<friend_id>')
-def lookup(friend_id):
-    query = "SELECT * FROM friends WHERE id = :specific_id"
-    data = {'specific_id': friend_id}
+
+
+
+@app.route('/friends/<friend_id>/edit', methods=['GET', 'POST'])
+def edit(friend_id):
+    query = "SELECT * FROM friends WHERE id = :id"
+    data = {'id': friend_id,}
+    mysql.query_db(query, data)
     friends = mysql.query_db(query, data)
-    return render_template('index.html', all_friends=friends, one_friend=friends[0])
+    return render_template('edit.html', all_friends=friends)
+
+
+
+
+
+
+
+
+@app.route('/friends/<friend_id>')
+def update(friend_id):
+    query = "UPDATE friends SET first_name = :first_name, last_name = :last_name, email = :email WHERE id = :id"
+    data = {
+             'first_name': request.form['first_name'], 
+             'last_name':  request.form['last_name'],
+             'email': request.form['email'],
+             'id': friend_id
+           }
+    mysql.query_db(query, data)
+    return redirect('/')
+
+@app.route('/friends/<friend_id>/delete')
+def delete(friend_id):
+    query = "DELETE FROM friends WHERE id = :id"
+    data = {'id': friend_id}
+    mysql.query_db(query, data)
+    return redirect('/')
+
 
 app.run(debug=True)
